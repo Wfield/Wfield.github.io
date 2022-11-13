7413aa809bb2dd53dea16aef336c054b0bc13a3f
bruce <xialong.li02@hand-china.com>
2022-11-13 21:40:33 +0800

    fix: update title

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ gallery.setAttribute('class', 'gallery');
 gallery.innerHTML = items;
 HomePage.appendChild(gallery);
 
-function createIframes(path, src) {
+function createIframes(path, src, desc) {
   if (path && src) {
     const iframe = document.createElement('iframe');
     iframe.setAttribute('id', path);
@@ -23,19 +23,21 @@ function createIframes(path, src) {
     iframe.setAttribute('height', '100%');
     iframe.src = src;
     document.body.append(iframe);
+    document.title = desc;
     HomePage.hide();
   } else {
     const ele = document.querySelector('iframe');
     if(ele) {
       ele.remove()
     }
+    document.title = 'wfield blog';
     HomePage.show();
   }
 }
 
 function hashChangeCallback() {
-  const { path, src} = hashRouter.find(route => `#${route.path}` === location.hash) || {};
-  createIframes(path, src)
+  const { path, src, desc } = hashRouter.find(route => `#${route.path}` === location.hash) || {};
+  createIframes(path, src, desc);
 }
 
 window.addEventListener('hashchange', hashChangeCallback);
